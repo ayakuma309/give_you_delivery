07116c70e4b01fa35266f4e93a5b09e11a4751c7
ayakuma309 <ayako.30901@gmail.com>
2022-12-28 19:27:47 +0900

    add: テーマ設定

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
+    './app/assets/stylesheets/**/*.css',
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
@@ -18,5 +19,21 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-  ]
+    require("daisyui")
+  ],
+  daisyui: {
+    themes: [
+      {
+      mytheme: {
+        "primary": " #42A0FF",
+        "secondary": " #42A0FF",
+        "accent": "#81D0CB",
+        "neutral": "#191A3E",
+        "base-100": "#FFFFFF",
+        info:  "#FFDC5D",
+        "success": "#fef08a",
+      },
+    },
+  ],
+  },
 }
