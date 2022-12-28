c0b329e39311f4101a59c8475e564f6838625232
ayakuma309 <ayako.30901@gmail.com>
2022-12-28 16:17:05 +0900

    add: RSpecの初期設定を完了

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,11 @@ module GiveYouDelivery
       g.skip_routes true
       g.assets false
       g.helper false
-      g.test_framework false
+      g.test_framework :rspec, # ここから5行を追記
+        fixtures: false, # テストDBにレコードを作るfixtureの作成をスキップ(FactoryBotを使用するため)
+        view_specs: false, # ビューファイル用のスペックを作成しない
+        helper_specs: false, # ヘルパーファイル用のスペックを作成しない
+        routing_specs: false # routes.rb用のスペックファイル作成しない
     end
   end
 end
