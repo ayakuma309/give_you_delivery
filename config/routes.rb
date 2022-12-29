e283481c8d679af8a4f8a655800a7ea53663ea26
ayakuma309 <ayako.30901@gmail.com>
2022-12-29 19:20:47 +0900

    add: itemの一覧画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   post "oauth/callback", to: "oauths#callback"
   get "oauth/callback", to: "oauths#callback"
   get "oauth/:provider", to: "oauths#oauth", as: :auth_at_provider
+
+  resources :items, only: %i[index]
 end
