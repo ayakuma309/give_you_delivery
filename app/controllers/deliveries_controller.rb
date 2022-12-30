c39f29b399a3210089a88e5bb117675a6642ddf4
ayakuma309 <ayako.30901@gmail.com>
2022-12-30 10:08:20 +0900

    add: Deliverly,詳細画面

---
+++ app/controllers/deliveries_controller.rb
@@ -0,0 +1,35 @@
+class DeliveriesController < ApplicationController
+  def index
+    @deliveries = current_user.deliveries
+  end
+
+  def show
+    @delivery = current_user.deliveries.find(params[:id])
+    @item = Item.find_by(id: @delivery.item_id)
+  end
+
+  def create
+    @delivery = current_user.deliveries.new(delivery_params)
+    if @delivery.save
+      redirect_to delivery_path(@delivery), success: t('defaults.message.created', item: Delivery.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_created', item: Delivery.model_name.human)
+      render :index
+    end
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def delivery_params
+    params.require(:delivery).permit(:item_id, :comment, :addressee, :addresser)
+  end
+
+end
