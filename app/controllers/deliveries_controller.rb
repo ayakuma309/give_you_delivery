583b84faabf3af49991693f8b3e28714f2d6faa8
ayakuma309 <ayako.30901@gmail.com>
2022-12-31 10:42:00 +0900

    add: edit,destroyアクション追加

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -1,11 +1,13 @@
 class DeliveriesController < ApplicationController
   skip_before_action :require_login, only: %i[show]
+  before_action :set_delivery, only: %i[edit update destroy]
+
   def index
     @deliveries = current_user.deliveries
   end
 
   def show
-    @delivery = current_user.deliveries.find(params[:id])
+    @delivery = Delivery.find(params[:id])
   end
 
   def create
@@ -19,15 +21,26 @@ class DeliveriesController < ApplicationController
     end
   end
 
-  def update
+  def edit; end
 
+  def update
+    if @delivery.update(delivery_params)
+      redirect_to delivery_path(@delivery), success: t('defaults.message.updated', item: Delivery.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_updated', item: Delivery.model_name.human)
+      render :edit
+    end
   end
 
   def destroy
-
+    @delivery.destroy!
+    redirect_to deliveries_path, success: t('defaults.message.deleted', item: Delivery.model_name.human)
   end
 
   private
+  def set_delivery
+    @delivery = current_user.deliveries.find(params[:id])
+  end
 
   def delivery_params
     params.require(:delivery).permit(:item_id, :comment, :addressee, :addresser)
