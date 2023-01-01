36e8e57d110d8e79de7bef22eecb164d01673f89
ayakuma309 <ayako.30901@gmail.com>
2023-01-01 23:27:50 +0900

    add: admin tag　一覧

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,17 +1,33 @@
 class Admin::TagsController < ApplicationController
+  before_action :set_tag, only: %i[edit update destroy]
+
   def index
-
+    @tags = Tag.all
   end
 
-  def edit
-
-  end
+  def edit; end
 
   def update
-
+    if @tag.update(tag_params)
+      redirect_to admin_tags_path, success: t('defaults.message.updated', item: Tag.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_created', item: Tag.model_name.human)
+      render :edit
+    end
   end
 
   def destroy
+    @tag.destroy!
+    redirect_to admin_tags_path, success: t('defaults.message.deleted', item: Tag.model_name.human)
+  end
 
+  private
+
+  def set_tag
+    @tag = Tag.find(params[:id])
+  end
+
+  def tag_params
+    params.require(:tag).permit(:name)
   end
 end
