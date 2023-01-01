c4b10290508f9962d4ef99595f5b2f3db71185bf
ayakuma309 <ayako.30901@gmail.com>
2023-01-01 18:57:46 +0900

    add: モデル、紐付け

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
   has_many :deliveries, dependent: :destroy
 
+  has_many :item_tags, dependent: :destroy
+  has_many :tags, through: :item_tags
+
   validates :name, presence: true
   validates :image, presence: true
 end
