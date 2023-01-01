7abfa6de7554b7dbb46d1ee042bd7e94dfc1ccbf
ayakuma309 <ayako.30901@gmail.com>
2023-01-01 17:07:31 +0900

    add: 判別のためのカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_29_132700) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_01_080450) do
   create_table "authentications", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "provider", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_29_132700) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
