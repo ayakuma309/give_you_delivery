dbf8d30887cae4df9ec38963bed659ce6242f30c
ayakuma309 <ayako.30901@gmail.com>
2023-01-01 17:54:54 +0900

    admin/itemsコントローラー

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,48 @@
+class Admin::ItemsController < Admin::BaseController
+  before_action :set_item, only: %w[edit update destroy]
+
+  def index
+    @items = Item.all.order(created_at: :desc)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path, success: t('defaults.message.created', item: Item.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_created', item: Item.model_name.human)
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @item.update(item_params)
+      redirect_to admin_items_path, success: t('defaults.message.updated', item: Item.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_updated', item: Item.model_name.human)
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @item.destroy!
+    redirect_to admin_items_path, success: t('defaults.message.deleted', item: Item.model_name.human)
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :image)
+  end
+end
